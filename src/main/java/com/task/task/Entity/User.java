369cfdb785b13e24592cae911293cd2e1687a991
Mickey <michaelzewdu1996@gmail.com>
2025-02-11 12:27:50 +0300

    added a new entity to keep track of users that have created account

---
+++ src/main/java/com/task/task/Entity/User.java
@@ -0,0 +1,56 @@
+package com.task.task.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "User")
+public class User {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column(name = "username")
+    private String username;
+    
+    @Column(name = "password")
+    private String password;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+}
